f3d079054afb94f73b496d65a1851744a29f4d7d
Michael Joly <michaeljoly6@gmail.com>
2017-03-10 12:18:03 +0100

    class map tout en un

--- src/GameBundle/Controller/DefaultController.php
+++ src/GameBundle/Controller/DefaultController.php
@@ -7,8 +7,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use GameBundle\Model\Decor;
 use GameBundle\Model\Personage;
 use GameBundle\Model\Map;
+use Symfony\Component\BrowserKit\Request;
 
 class DefaultController extends Controller {
+
+    private $decor1;
+    private $perso1;
 
     /**
      * @Route("/")
@@ -21,40 +25,27 @@ class DefaultController extends Controller {
      * @Route("/tests")
      */
     public function testsAction() {
+        $this->oMap = new Map();
+        $unserFile = $this->oMap->load();
+        $this->oMap = $unserFile;
 
-        // load decor datas
-        $decor1 = new Decor();
-        $decor1->setPositionX(123);
-        $decor1->setPositionY(22);
-        $decor1->setType('mur'); // wall
-        $decor1->setImage('wall.gif');
-
-        $decor2 = new Decor();
-        $decor2->setPositionX(72);
-        $decor2->setPositionY(44);
-        $decor2->setType('mur'); // wall
-        $decor2->setImage('wall.gif');
-
-        $perso1 = new Personage();
-        $perso1->setPositionX(155);
-        $perso1->setPositionY(175);
-        $perso1->setType('hero'); // perso
-        //$perso1->setImage('war.gif');
-
-        $oMap = new Map();
-        $oMap->addElement($decor1);
-        $oMap->addElement($decor2);
-
-        $oMap->addElement($perso1);
-
-        dump($decor1);
-        dump($decor2);
-
-        dump($oMap->getaElements());
-
-        //$oMap->collision($perso1, $decor2);
-
-        return $this->render('GameBundle:Map:initmap.html.twig', array('map' => $oMap->getaElements()));
+        return $this->render('GameBundle:Map:template.html.twig', array('map' => $this->oMap->getaElements()));
     }
+    /**
+     * @Route("/tests/move")
+     */
+    public function testsMoveAction()
+    {
+        $moveDirection = $_GET['move'];
+
+        $this->oMap = new Map();
+        $unserFile = $this->oMap->load();
+        $this->oMap = $unserFile;
+
+        $this->oMap->move($moveDirection);
+
+        return $this->render('GameBundle:Map:initmap.html.twig', array('map' => $this->oMap->getaElements()));
+    }
+
 
 }
--- src/GameBundle/Model/Map.php
+++ src/GameBundle/Model/Map.php
@@ -4,6 +4,14 @@ namespace GameBundle\Model;
 
 class Map {
 
+    public static $_MOVEUP = 1;
+    public static $_MOVERIGHT = 2;
+    public static $_MOVEDOWN = 3;
+    public static $_MOVELEFT = 4;
+    public static $_OFFSETMOVE = 4; //pixels
+    //private $moveDirection;
+    private $mapMaxX = 320;
+    private $mapMaxY = 480;
     protected $aElements = [];
 
     /*
@@ -27,5 +35,163 @@ class Map {
     protected function setaElements($structure) {
         $this->aElements = $structure;
     }
+
+    private function createAction() { // create a new map in a file
+        // load decor datas
+        $this->decor1 = new Decor();
+        $this->decor1->setPositionX(110);
+        $this->decor1->setPositionY(110);
+        $this->decor1->setType('mur'); // wall
+        $this->decor1->setImage('wall.gif');
+        /*
+                $decor2 = new Decor();
+                $decor2->setPositionX(123);
+                $decor2->setPositionY(22);
+                $decor2->setType('mur'); // wall
+                $decor2->setImage('wall.gif');
+        */
+        $this->perso1 = new Personage();
+        $this->perso1->setPositionX(210);
+        $this->perso1->setPositionY(190);
+        $this->perso1->setType('hero'); // perso
+
+        $this->oMap = new Map();
+        $this->oMap->addElement($this->perso1);
+
+        $this->oMap->addElement($this->decor1);
+
+        //dump($decor1);
+        //dump($decor2);
+
+        //dump($oMap->getaElements());
+        //$this->save(new);
+    }
+
+
+    public function load() {
+        if(file_exists('map1move')) {
+            $filenameSer = 'map1move';
+        } else if(file_exists('map1')) {
+            $filenameSer = 'map1';
+        }
+        else {
+            // create map1
+        }
+        $contentFile = file_get_contents($filenameSer);
+        $unserFile = unserialize($contentFile);
+
+        return($unserFile);
+    }
+
+    public function save() {
+        $ser = serialize($this);
+        $fileNameSer = 'map1move';
+        file_put_contents($fileNameSer, $ser);
+    }
+
+    public function collision($moveDirection){
+        dump('debut collision test');
+        //Comparaison Y1/Y2 & X1/X2
+        $x1 = $this->aElements[0]->getPositionX();
+        $y1 = $this->aElements[0]->getPositionY();
+        $x2 = $this->aElements[1]->getPositionX();
+        $y2 = $this->aElements[1]->getPositionY();
+        /*
+        A1(x1,      y1)       <   C2(x2+32,   y2+32)
+        B1(x1+32,   y1)       <   D2(x2,      y2+32)
+        C1(x1+32,   y1+32)    <   A2(x2,      y2)
+        D1(x1,      y1+32)    <   B2(x2+32,   y2)   //pour x et pour y
+        */
+        if ($moveDirection == self::$_MOVELEFT || $moveDirection == self::$_MOVEUP) {
+            if ((($x1 >= $x2) && ($x1 <= ($x2 + 32))) && (($y1 >= $y2) && ($y1 <= ($y2 + 32)))) {
+                dump('collision entre A1 et C2!');
+                return false;
+            }
+        }
+
+        if ($moveDirection == self::$_MOVERIGHT || $moveDirection == self::$_MOVEUP) {
+            if (((($x1 + 32) >= $x2) && (($x1 + 32) <= $x2 + 32)) && (($y1 >= $y2) && ($y1 <= ($y2 + 32)))) {
+                dump('collision entre B1 et D2!');
+
+                return false;
+            }
+        }
+
+        if ($moveDirection == self::$_MOVERIGHT || $moveDirection == self::$_MOVEDOWN) {
+            if ((($x1 + 32 >= $x2) && (($x1 + 32) <= ($x2 + 32))) && ((($y1 + 32) <= ($y2 + 32)) && (($y1 + 32) >= $y2))) {
+                dump('collision entre C1 et A2!');
+
+                return false;
+            }
+        }
+        if ($moveDirection == self::$_MOVELEFT || $moveDirection ==  self::$_MOVEDOWN) {
+            if ((($x1 <= $x2 + 32) && ($x1 >= $x2)) && ((($y1 + 32) >= $y2) && (($y1 + 32) <= ($y2 + 32)))) {
+                dump('collision entre D1 et B2!');
+
+                return false;
+            }
+        }
+        dump('no collision');
+        return true;
+
+
+    }
+
+    public function move($moveDirection) {
+        //dump($moveDirection);
+        if ($this->moveable($moveDirection)) {
+            switch ($moveDirection) {
+                case self::$_MOVEUP :
+                    $this->aElements[0]->setPositionY($this->aElements[0]->getPositionY() - self::$_OFFSETMOVE);
+                    break;
+                case self::$_MOVERIGHT :
+                    $this->aElements[0]->setPositionX($this->aElements[0]->getPositionX() + self::$_OFFSETMOVE);
+                    break;
+                case self::$_MOVEDOWN :
+                    $this->aElements[0]->setPositionY($this->aElements[0]->getPositionY() + self::$_OFFSETMOVE);
+                    break;
+                case self::$_MOVELEFT :
+                    $this->aElements[0]->setPositionX($this->aElements[0]->getPositionX() - self::$_OFFSETMOVE);
+                    break;
+            } // end switch
+            //save
+            $this->save();
+        }
+    }
+
+    public function moveable($moveDirection) {
+        //return true;
+        switch ($moveDirection) {
+            case self::$_MOVEUP :
+                $condition = ($this->aElements[0]->getPositionY() - self::$_OFFSETMOVE >= 0);
+                if (!$condition) {
+                    //  $this->posPersoY = $this->mapMaxY - 2;
+                }
+                break;
+            case self::$_MOVERIGHT :
+                $condition = ($this->aElements[0]->getPositionX() + self::$_OFFSETMOVE <= $this->mapMaxX);
+                if (!$condition) {
+                    // $this->posPersoX = $this->mapMaxX - 2;
+                }
+                break;
+            case self::$_MOVEDOWN :
+                $condition = ($this->aElements[0]->getPositionY() + 32 + self::$_OFFSETMOVE <= $this->mapMaxY);
+                if (!$condition) {
+                    // $this->posPersoX = $this->mapMaxY + 2;
+                }
+                break;
+            case self::$_MOVELEFT :
+                $condition = ($this->aElements[0]->getPositionX() - self::$_OFFSETMOVE >= 0);
+                if (!$condition) {
+                    // $this->posPersoX = $this->mapMaxX + 2;
+                }
+                break;
+        } // end switch
+
+            return ($condition && ($this->collision($moveDirection)));
+    }
+
+
+
 
 }
