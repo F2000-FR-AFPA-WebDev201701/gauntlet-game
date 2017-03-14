87bc9dc35cb7c366f8204b62b8399594a4fd6347
Hach3n <huguesnicolas@gmail.com>
2017-03-14 11:43:37 +0100

    commit de la mort qui tue

--- src/GameBundle/Controller/UserController.php
+++ src/GameBundle/Controller/UserController.php
@@ -2,30 +2,65 @@
 
 namespace GameBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use GameBundle\Entity\User;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use GameBundle\Form\RegisterType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller {
 
     /**
-     * @Route("/login/register")
+     * @Route("/user", name="login")
      */
-    public function registerAction() {
+    public function loginAction(Request $request) {
         $user = new User;
         $form = $this->createForm(RegisterType::class, $user);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $repo = $this->getDoctrine()->getRepository('GameBundle:User');
+
+            $userValid = $repo->findOneBy(array(
+                'login' => $user->getPseudo(),
+                'password' => $user->getPassword(),
+            ));
+
+            if ($userValid) {
+                $request->getSession()->set("user", $userValid);
+                $request->getSession()->getFlashBag()->add('success', "Connecté");
+            } else {
+                $request->getSession()->getFlashBag()->add('error', 'Identifiants incorrects');
+            }
+        }
+        //dump($form->createView());
+        return $this->render('GameBundle:Default:index.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/login/register", name="register")
+     */
+    public function registerAction(Request $request) {
+        $user = new User;
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->remove('password');
+        $form->handleRequest($request);
+
+
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getConfirmPassword());
+            $user->setPassword($password);
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
-
-            return $this->rend('GameBundle:Default:_register.html.twig', [
-                        'form' => $form->createViews()
-            ]);
         }
+        return $this->render('GameBundle:Default:_register.html.twig', ['form' => $form->createView()]);
     }
 
 }
--- src/GameBundle/Form/RegisterType.php
+++ src/GameBundle/Form/RegisterType.php
@@ -1,29 +1,40 @@
 <?php
 
-use GameBundle\Entity\User;
-
 namespace GameBundle\Form;
+
+use GameBundle\Entity\User;
+//use Symfony\Component\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RegisterType extends AbstractType {
 
-    public function registerForm(FormBuilder $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('pseudo', 'text', array('max_lenght' => 12, 'required' => true, label => 'pseudo'))
-                ->add('password', 'password', array('max_lenght' => 12, 'required' => true, label => 'password'))
-                ->add('email', 'email', array('max_lenght' => 50, 'required' => true, 'label' => 'email'))
-                ->add('password', 'repeated', array(
-                    'type' => 'password',
-                    'invalid_message' => 'les mots de passe ne correspondent pas',
-                    'options' => array('label' => 'password'),
-                    'first_name' => 'password',
-                    'second_name' => 'passwordconfirm'
-        ));
+                ->add('pseudo', TextType::class)//, array('max_lenght' => 12, 'required' => true, 'label' => 'pseudo'))
+                ->add('password', PasswordType::class)//, array('max_lenght' => 12, 'required' => true, 'label' => 'password'))
+                ->add('email', EmailType::class)//, array('max_lenght' => 50, 'required' => true, 'label' => 'email'))
+                ->add('confirmPassword', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'The password fields must match.',
+                    'options' => array('attr' => array('class' => 'password-field')),
+                    'required' => true,
+                    'first_options' => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Repeat Password'),
+                ))
+                ->add('submit', SubmitType::class);
     }
 
-    public function getDefaultOptions(array $options) {
-        return array(
-            'data_class' => 'Wmd\WatchMyDeskBundle\Entity\User'
-        );
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+        ));
     }
 
     public function getName() {
