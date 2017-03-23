d6efb5d364295ba961edc72010ca0e63a5a57cb3
FORMATEUR <henryn1976@gmail.com>
2017-03-23 10:08:36 +0100

    Save Characters datas (next level)

--- src/GameBundle/Controller/GameController.php
+++ src/GameBundle/Controller/GameController.php
@@ -173,27 +173,29 @@ class GameController extends Controller {
                 }
 
                 //next level
-                if ($oMapUnser->isNextlvl()){
-                    $nextLevel = $oMapUnser->getCurrentLvl()+1;
+                if ($oMapUnser->isNextlvl()) {
+                    $nextLevel = $oMapUnser->getCurrentLvl() + 1;
                     // test if the end ?
-                    if($nextLevel > $oMapUnser->nbMaps()) {
+                    if ($nextLevel > $oMapUnser->nbMaps()) {
                         return $this->render('GameBundle:Map:win.html.twig');
                     }
                     // no end the go to next level
                     $oMap = new Map();
-                    $initMapSer = $oMap->load($nextLevel); // load map from file (mapX.initial)
-                    $oGame->setSaveGame($initMapSer);
+                    $initMapSerNext = $oMap->load($nextLevel); // load map from file (mapX.initial)
+                    $oMapUnserNext = unserialize($initMapSer);
+                    $oMapUnserNext->setaElementsCharacters($oMapUnser->getaElementsCharacters()); // save hp & score...
+                    $initMapSerNext = serialize($oMapUnserNext);
+                    $oGame->setSaveGame($initMapSerNext);
                     $oGame->setStatus(1);
-                    $oMapUnser = unserialize($initMapSer);                  
                 }
-                
+
                 $repo->flush(); // save the game into database
-    
+
                 return $this->render('GameBundle:Map:map.html.twig', array(
-                                'map' => $oMapUnser->getaElements(),
-                                'player' => $oMapUnser->getaElementsCharacters()[0]
-                ));  
-           
+                            'map' => $oMapUnser->getaElements(),
+                            'player' => $oMapUnser->getaElementsCharacters()[0]
+                ));
+
             case 'shoot':
                 $oMapUnser = unserialize($oGame->getSaveGame()); // get a map object
                 $oMapUnser->attack();
